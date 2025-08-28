e2581d8f30a7f4c0748145e4b63042155a5f7e4f
Berkay2707 <bniziplioglu8111@gmail.com>
2025-08-29 00:50:00 +0300

    C# ödevi yapıldı
    3 maddeden oluşan C# ödevi yapıldı

---
+++ C# Ödev 1/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Ödev_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1- İki sayının toplamını yazan programı yazınız (Tamamlandı)
+            //2 - Kullanıcının girdiği iki sayının kareleri toplamını ekrana yazdırınız (Tamamlandı)
+            //3 - 1'den 100'e kadar olan sayıların küplerini bulan programı yazınız 
+
+            //1
+            #region
+            //int Sayı1;
+            //int Sayı2;
+            //int Toplam;
+
+            //Console.WriteLine("1. Sayınızı giriniz");
+            //Sayı1 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine(" ");
+
+            //Console.WriteLine("2. Sayınızı giriniz");
+            //Sayı2= Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine(" ");
+
+            //Toplam = Sayı1 + Sayı2;
+
+            //Console.WriteLine("Sayı 1 ve Sayı 2'nin Toplamı = "+ Toplam);
+            #endregion
+
+            //2
+            #region
+
+            //int Sayı1;
+            //int Sayı1_Karesi;
+
+            //int Sayı2;
+            //int Sayı2_Karesi;
+
+            //int Toplam;
+
+            //Console.WriteLine("1.Sayıyı giriniz ");
+            //Sayı1=Convert.ToInt32(Console.ReadLine());
+
+            //Sayı1_Karesi = Sayı1 * Sayı1;
+
+            //Console.WriteLine(" ");
+
+            //Console.WriteLine("2. Sayıyı giriniz ");
+            //Sayı2 = Convert.ToInt32(Console.ReadLine());
+
+            //Sayı2_Karesi = Sayı2 * Sayı2;
+
+            //Toplam = Sayı1_Karesi + Sayı2_Karesi;
+
+            //Console.WriteLine( " ");
+
+            //Console.WriteLine("1. Sayınızın karesi = "+  Sayı1_Karesi);
+
+            //Console.WriteLine("2. Sanınızın karesi = " + Sayı2_Karesi);
+
+            //Console.WriteLine(" ");
+
+            //Console.WriteLine("Karelerin Toplamı = "+ Toplam);
+
+
+
+
+
+
+            #endregion
+
+            //3
+            #region
+
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //    Console.WriteLine(i*i*i);
+            //}
+
+            #endregion
+
+            Console.ReadLine();
+        }
+    }
+}
